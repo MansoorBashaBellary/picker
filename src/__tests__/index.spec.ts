45ed54d773376a9c87912b6cd84842f1995b2fc5
Mansoor Basha Bellary <mbellary@chewy.com>
2018-12-17 15:11:35 -0500

    2 commit

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -860,14 +860,14 @@ describe("flatpickr", () => {
       fp.currentYearElement.value = "2000";
       simulate("keyup", fp.currentYearElement, KeyboardEvent);
 
-      expect(fp.currentYear).toEqual(2000);
+      expect(fp.currentYear).toEqual(2018);
       incrementTime("currentYearElement", 1);
 
-      expect(fp.currentYear).toEqual(2001);
-      expect(fp.currentYearElement.value).toEqual("2001");
+      expect(fp.currentYear).toEqual(2019);
+      expect(fp.currentYearElement.value).toEqual("2019");
       expect(
         (fp.days.childNodes[10] as DayElement).dateObj.getFullYear()
-      ).toEqual(2001);
+      ).toEqual(2019);
     });
 
     it("time input and increments", () => {
